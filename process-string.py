d1237669b52cef5ac5de21df8116beb7176ecbd0
Mork Sunhout <156296906+MorkSunhout@users.noreply.github.com>
2024-01-29 15:32:59 +0700

    process-string.py

--- process-string.py
+++ process-string.py
@@ -1,3 +1,4 @@
+
 # Application 1
 # Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
 
@@ -6,6 +7,11 @@
 # * "Hello World" -> "HHeelllloo  WWoorrlldd"
 # * "1234!_ "     -> "11223344!!__  "
 
+word = input("Please enter any character: ")
+doubled_word = ""
+for char in word:
+    doubled_word += char * 2
+print(doubled_word)
 
 # Application 2
 #  Given a string indicating a range of letters, return a string which includes all the letters in that range, including the last letter. Note that if the range is given in capital letters, return the string in capitals also!
@@ -18,7 +24,24 @@
 # Notes A hyphen will separate the two letters in the string.
 
 
-alphabet = "abcdefghijklmnopqrstuvwxyz"
+# alphabet = "abcdefghijklmnopqrstuvwxyz"
+# start, end = alphabet.split('-')
+# user_range = input("Enter a range of letters (e.g., a-z): ")
+
+def generate_letter_range(user_range):
+    start, end = user_range.split('-')
+    start_index = ord(start)
+    end_index = ord(end)
+
+    result = ''.join(chr(i) for i in range(start_index, end_index + 1))
+
+    return result.upper() if start.isupper() else result
+
 user_range = input("Enter a range of letters (e.g., a-z): ")
+result = generate_letter_range(user_range)
+print(result)
+
+
+
 
 
